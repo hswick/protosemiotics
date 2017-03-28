577c6254a7e55c12939840585a4e8f9480fcbd42
hswick <hswick@smu.edu>
2017-03-27 22:16:26 -0500

    Deleted selective breeding

--- breed_networks.rb
+++ breed_networks.rb
@@ -39,21 +39,21 @@ def pop_mix_optimized(population)
 	output
 end
 
-def selective_breeding(org1, org2, n)
-	offspring = []
-	for i in 0..n
-		result = crossover_gene_matrices(org1.to_genes, org2.to_genes)
-		offspring << result[0]
-		offspring << result[1]
-	end
-	offspring
-end
-
 def birth(genes)
 	if rand(100) >= 4#0.01 probability because 4 values of genes
 		mutation!(genes[rand(genes.length)])
 	end
 	Network.new.from_genes(genes)
+end
+
+def selective_breeding(org1, org2, n)
+	offspring = []
+	for i in 0..n
+		result = crossover_gene_matrices(org1.to_genes, org2.to_genes)
+		offspring << birth(result[0])
+		offspring << birth(result[1])
+	end
+	offspring
 end
 
 #Always breeds two offspring, byproduct of crossover
@@ -94,6 +94,7 @@ def basic_genetic_algo
 		simulation_scores = fitnesses.map {|fitness_net| fitness_net[0][0]}	
 
 		puts "Finished generation " + i.to_s
+		puts "Max simulation score " + simulation_scores.max.to_s
 		puts "Average simulation score " + mean(simulation_scores).to_s
 		puts "Average node count " + mean(population.map{|n|n.nodes.length}).to_s
 		puts "Average edge count " + mean(population.map{|n|n.edges.length}).to_s
@@ -115,52 +116,4 @@ def get_top_two(population)
 	fitnesses.map {|fitness_net| population[fitness_net[1]]}
 end
 
-def ga(population, population_count)
-			new_population = pop_mix_optimized(population)	
-
-			fitness_net_tuples = test_population_fitness(new_population)	
-
-					#sort population
-			fitnesses = fitness_net_tuples.sort_by { |fitness_net|
-				[-fitness_net[0][0], fitness_net[0][1]]#Default sorting ascending
-			}.slice(0, population_count)	
-
-		fitnesses.map {|fitness_net| population[fitness_net[1]]}
-end
-
-def diverse_top_breeding
-	population_count = 200
-
-	population = init_population(population_count)
-
-	for i in 0..2
-		top_two = get_top_two(population)
-
-		population = selective_breeding(top_two[0], top_two[1], population_count)
-
-		separate_population_count = 10
-
-		#init populations
-		populations = []
-		for j in 0..separate_population_count
-			populations << population.clone
-		end
-
-		#evolve populations separately(stochastic multithreaded part)
-		populations.each do |population|
-			for k in 0..10
-				population = ga(population, population_count)
-			end
-		end
-
-		diverse_top_population = []
-
-		populations.each do |population|
-			top_two = get_top_two(population)
-			diverse_top_population << top_two[0]
-			diverse_top_population << top_two[1]
-		end
-
-		population = ga(diverse_top_population, population_count)#Average them together then select top
-	end
-end
+basic_genetic_algo
